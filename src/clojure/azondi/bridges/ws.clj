db7057f0df341da8b39f70837dc17f9775d8abd6
Michael Klishin <michael@clojurewerkz.org>
2014-03-16 02:36:58 +0400

    WS bridge propagates messages as JSON

--- src/clojure/azondi/bridges/ws.clj
+++ src/clojure/azondi/bridges/ws.clj
@@ -1,12 +1,28 @@
 (ns azondi.bridges.ws
   (:require [taoensso.timbre :as log]
             [org.httpkit.server :refer [run-server with-channel send! on-close]]
-            [compojure.core :refer [defroutes GET POST]]
+            [compojure.core :refer [routes GET POST]]
             [clojurewerkz.meltdown.reactor :as mr]
             [clojurewerkz.meltdown.selectors :refer [match-all]]
-            [clojurewerkz.meltdown.consumers :as mc])
+            [clojurewerkz.meltdown.consumers :as mc]
+            [cheshire.core :as json])
   (:import jig.Lifecycle))
 
+(defn welcome-message
+  []
+  (json/generate-string {:type "welcome"}))
+
+(defn send-welcome-message
+  [ws]
+  (send! ws (welcome-message)))
+
+(defn event-message
+  [^String topic ^bytes payload]
+  (json/generate-string {:type "event" :topic topic :payload (String. payload "UTF-8")}))
+
+(defn send-event-message
+  [ws ^String topic payload]
+  (send! ws (event-message topic payload)))
 
 (defn ws-connection-handler
   [req clients reactor]
@@ -14,12 +30,12 @@
     (log/infof "Accepted WebSocket bridge connection from %s" (:remote-addr req))
     (swap! clients conj ws)
     (let [sub (mr/on reactor (match-all) (fn [evt]
-                                           (send! ws (:data evt))))]
+                                           (send-event-message ws (:key evt) (:data evt))))]
       (on-close ws (fn [status]
                      (swap! clients disj ws)
                      (mc/cancel sub)
                      (log/infof "WebSocket bridge connection from %s is closed, status: %s" (:remote-addr req) status))))
-    (send! ws "You are connected to WS bridge")))
+    (send-welcome-message ws)))
 
 (deftype WebSocketBridge [config]
   Lifecycle
@@ -31,7 +47,7 @@
           clients (atom #{})
           ;; define routes here so that they have access to
           ;; clients, reactor, etc. MK.
-          routes  (defroutes ws-routes
+          routes  (routes
                     (GET  "/events/stream" req (ws-connection-handler req clients r)))
           server (run-server routes {:port port})]
       (log/infof "About to start WebSocket/polling bridge server on port %d" port)
