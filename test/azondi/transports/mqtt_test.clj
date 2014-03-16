aef9c90e187d6144de79e41cedc38ec9979aec86
Michael Klishin <michael@clojurewerkz.org>
2014-03-16 16:58:17 +0400

    MQTT transport connection test

--- test/azondi/transports/mqtt_test.clj
+++ test/azondi/transports/mqtt_test.clj
@@ -1,7 +1,10 @@
 (ns azondi.transports.mqtt-test
   (:require [clojure.test :refer :all]
             [azondi.transports.mqtt :as mqtt]
-            [clojurewerkz.triennium.mqtt :as tr]))
+            [clojurewerkz.triennium.mqtt :as tr]
+            [clojurewerkz.machine-head.client :as mh]
+            [clojurewerkz.machine-head.durability :as md]
+            [user :as u]))
 
 (deftest test-record-subscribers
   (let [ctx :ctx ;; stub
@@ -27,3 +30,16 @@
                                                                        :topic "a/topic"
                                                                        :qos 0}}}}}
            @m))))
+
+(def connection-opts {:clean-sesion true
+                      :username "michael@example.org"
+                      :password "michael-pwd"})
+
+(deftest test-mqtt-transport-start
+  (u/go)
+  (let [id (mh/generate-id)
+        c  (mh/connect "tcp://127.0.0.1:1883" id
+                       (md/new-memory-persister)
+                       connection-opts)]
+    (is (mh/connected? c)))
+  (u/stop))
