da146e030127c9770d848abc272f9e5a5e22de01
Michael Klishin <michael@clojurewerkz.org>
2014-03-16 19:29:43 +0400

    Check individual values instead of comparing records

--- test/azondi/transports/mqtt_test.clj
+++ test/azondi/transports/mqtt_test.clj
@@ -5,20 +5,24 @@
             [clojurewerkz.machine-head.client :as mh]
             [clojurewerkz.machine-head.durability :as md]
             [user :as u])
-  (:import java.util.concurrent.atomic.AtomicInteger))
+  (:import java.util.concurrent.atomic.AtomicInteger
+           java.util.UUID))
 
 (deftest test-record-subscribers
   (let [ctx :ctx ;; stub
         xs  [["a/topic" 0] ["b/topic" 0]]
         m   (ref (tr/make-trie))]
     (dosync (alter m mqtt/record-subscribers ctx xs))
-    (is (= {"b" {"topic" {:values #{#azondi.transports.mqtt.Subscriber{:ctx :ctx
-                                                                       :topic "b/topic"
-                                                                       :qos 0}}}}
-            "a" {"topic" {:values #{#azondi.transports.mqtt.Subscriber{:ctx :ctx
-                                                                       :topic "a/topic"
-                                                                       :qos 0}}}}}
-           @m))))
+    (let [va (first (get-in @m ["a" "topic" :values]))
+          vb (last  (get-in @m ["b" "topic" :values]))]
+      (are [k v] (is (= v (k va)))
+           :ctx   :ctx
+           :topic "a/topic"
+           :qos   0)
+      (are [k v] (is (= v (k vb)))
+           :ctx   :ctx
+           :topic "b/topic"
+           :qos   0))))
 
 (deftest test-unrecord-subscribers
   (let [ctx :ctx ;; stub
@@ -27,16 +31,17 @@
     (dosync
      (alter m mqtt/record-subscribers ctx xs)
      (alter m mqtt/unrecord-subscribers ctx ["b/topic"]))
-    (is (= {"a" {"topic" {:values #{#azondi.transports.mqtt.Subscriber{:ctx :ctx
-                                                                       :topic "a/topic"
-                                                                       :qos 0}}}}}
-           @m))))
+    (let [v (first (get-in @m ["a" "topic" :values]))]
+      (are [k v'] (is (= v' (k v)))
+           :ctx   :ctx
+           :topic "a/topic"
+           :qos   0))))
 
 (def connection-opts {:clean-sesion true
                       :username "michael@example.org"
                       :password "michael-pwd"})
 
-(deftest test-mqtt-transport-start
+(deftest test-connect-disconnect
   (u/go)
   (let [id (mh/generate-id)
         c  (mh/connect "tcp://127.0.0.1:1883" id
@@ -44,6 +49,17 @@
                        connection-opts)]
     (is (mh/connected? c))
     (mh/disconnect-and-close c))
+  (u/stop))
+
+(deftest test-connection-with-invalid-credentials
+  (u/go)
+  (let [id (mh/generate-id)]
+    (is (thrown? org.eclipse.paho.client.mqttv3.MqttSecurityException
+                 (mh/connect "tcp://127.0.0.1:1883" id
+                             (md/new-memory-persister)
+                             {:clear-session true
+                              :username "d0es-n07 exist"
+                              :password (str (UUID/randomUUID))}))))
   (u/stop))
 
 (deftest test-basic-topic-subscription
