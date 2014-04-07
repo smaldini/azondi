8c8b80de356b701db099aba54f20d96dd3dc4c7b
Malcolm Sparks <malcolm@juxt.pro>
2014-04-07 12:03:08 +0100

    Fixing azondi reference in main

--- src/clojure/azondi/main.clj
+++ src/clojure/azondi/main.clj
@@ -7,7 +7,7 @@
   ;; We eval so that we don't AOT anything beyond this class
   (eval '(do (require 'azondi.system)
              (require 'com.stuartsierra.component)
-             (com.stuartsierra.component/start (abracadabra.system/new-system))
+             (com.stuartsierra.component/start (azondi.system/new-system))
              (println "Azondi - high-performance MQTT broker")
              (println "Copyright © 2014 OpenSensors.IO Ltd.")
              (println "Ready..."))))
