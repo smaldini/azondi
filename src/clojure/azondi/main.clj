130154cdd0056aca1b190576e8e690986b4beec5
Michael Klishin <michael@clojurewerkz.org>
2014-04-12 21:00:21 +0400

    Cosmetics

--- src/clojure/azondi/main.clj
+++ src/clojure/azondi/main.clj
@@ -1,14 +1,14 @@
 (ns azondi.main
+  "Main entry point"
+  (:require [com.stuartsierra.component :as component])
   (:gen-class))
-
-;; This is the main entry point for production. 'lein run' and 'java -jar uberjar'.
 
 (defn -main [& args]
   ;; We eval so that we don't AOT anything beyond this class
   (eval '(do (require 'azondi.system)
              (require 'com.stuartsierra.component)
              ;; TODO: Get from arguments
-             (com.stuartsierra.component/start (azondi.system/new-prod-system))
+             (component/start (azondi.system/new-prod-system))
              (println "Azondi: sensor information ingestion system")
              (println "Copyright © 2014 OpenSensors.IO Ltd.")
              (println "Ready..."))))
