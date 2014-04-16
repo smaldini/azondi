24580e8ea4989b0363969407caf162bc290b857b
Michael Klishin <michael@clojurewerkz.org>
2014-04-17 01:45:50 +0400

    Introduce azondi.devices

---
+++ src/clojure/azondi/devices.clj
@@ -0,0 +1,6 @@
+(ns azondi.devices
+  (:require [clojure.java.jdbc :as j]))
+
+(defn device-names
+  [conn ^String username]
+  (map :name (j/query conn ["SELECT name FROM devices WHERE owner = ?" username])))
