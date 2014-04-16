2d226976c5be4327bf9590498c716583627c80ac
Michael Klishin <michael@clojurewerkz.org>
2014-04-17 01:45:37 +0400

    Avoid spaces in device names

--- dev/src/sql/seed.sql
+++ dev/src/sql/seed.sql
@@ -25,17 +25,17 @@ DELETE FROM devices;
 
 INSERT INTO devices (client_id, name, device_id, description, latitude, longitude, owner, device_password_hash)
             -- password: device-1-pwd
-             VALUES ('3221e1gltf8vlofaf6to', 'device 1', '79f4572a-5493-486e-9abe-7337ea06ba4b', '',
+             VALUES ('3221e1gltf8vlofaf6to', 'device1', '79f4572a-5493-486e-9abe-7337ea06ba4b', '',
              51.50722, -0.12750, 'yods', '$s0$e0801$UFqu7r6NqzzA8f2Izbgjig==$D5QttKRnviio4CT8YON+m9OoesbDNen4r0c7P19pqsE=');
 
 INSERT INTO devices (client_id, name, device_id, description, latitude, longitude, owner, device_password_hash)
             -- password: device-2-pwd
-             VALUES ('asqv8fs0moo0kv7mknev9', 'device 2', '24f9eb8a-ffee-4ec6-b593-11bbcb749a86', '',
+             VALUES ('asqv8fs0moo0kv7mknev9', 'device2', '24f9eb8a-ffee-4ec6-b593-11bbcb749a86', '',
              51.50722, -0.12750, 'yods', '$s0$e0801$obspyjv/9zmFcD9we9A3oA==$fqeO5pvrR57aaqy4EAy9P3hZ+DS/yoT9GtfJnA3aLq4=');
 
 INSERT INTO devices (client_id, name, device_id, description, latitude, longitude, owner, device_password_hash)
             -- password: device-3-pwd
-             VALUES ('e4ih3bn0uqr89ehbodeas', 'device 3', 'd6acc822-f46a-4c45-88c0-4c29284fd4af', '',
+             VALUES ('e4ih3bn0uqr89ehbodeas', 'device3', 'd6acc822-f46a-4c45-88c0-4c29284fd4af', '',
              51.50722, -0.12750, 'yods', '$s0$e0801$EGG/OmC7ZEIUOkU9t/m5XQ==$oyI8bQInV1jeaMYaV4tHxleK3Hzarr4IrLGb6dA+Vtc=');
 
 --
