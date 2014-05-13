664ff81a35eda7b69663dbbd316661c6671d6d3c
Malcolm Sparks <malcolm@juxt.pro>
2014-05-14 00:46:26 +0100

    Seeding devices

--- dev/src/clojure/azondi/seed.clj
+++ dev/src/clojure/azondi/seed.clj
@@ -3,6 +3,7 @@
   (:require
    [com.stuartsierra.component :as component]
    [azondi.http :refer (request)]
+   [clojure.tools.logging :refer :all]
    #_[org.httpkit.client :refer (request) :rename {request http-request}]
    #_[cheshire.core :refer (encode decode)]
    #_[clojure.pprint :refer (pprint)]
@@ -16,14 +17,30 @@
   (str "http://localhost:" port "/api/1.0" (apply path-for routes k args)))
 
 (defn seed-data! [routes port]
-  (request :put (make-uri routes port :azondi.api/user :user "malcolm")
-           :data {:password "password"
-                  :name "Malcolm Sparks"
-                  :email "malcolm@juxt.pro"})
   (request :put (make-uri routes port :azondi.api/user :user "yods")
            :data {:password "password"
                   :name "Yodit Stanton"
                   :email "yodit@atomicdatalabs.com"})
+
+  (request :post (make-uri routes port :azondi.api/devices :user "yods")
+           :auth ["yods" "password"]
+           :data {:name "Arduino 1" :description "Some arduino lying around"}
+           :data {:name "Arduino 2" :description "A broken arduino"}
+           :data {:name "Pollution sensor" :description ""})
+
+  (request :put (make-uri routes port :azondi.api/user :user "malcolm")
+           :data {:password "password"
+                  :name "Malcolm Sparks"
+                  :email "malcolm@juxt.pro"})
+
+  (request :post (make-uri routes port :azondi.api/devices :user "malcolm")
+           :auth ["malcolm" "password"]
+           :data {:name "S3-1" :description "MQTTitude on Samsung Galaxy S3"})
+
+  (request :post (make-uri routes port :azondi.api/devices :user "malcolm")
+           :auth ["malcolm" "password"]
+           :data {:name "S3-2" :description "MQTTitude (test) on Samsung Galaxy S3"})
+
   (request :put (make-uri routes port :azondi.api/user :user "mk")
            :data {:password "password"
                   :name "Michael Klishin"
@@ -34,7 +51,9 @@
   (start [this]
     (let [routes (get-in this [:api :routes])
           port (get-in this [:webserver :port])]
-      (seed-data! routes port)
+      (try
+        (seed-data! routes port)
+        (catch Exception e (errorf e "Error seeding data")))
       this
       ))
   (stop [this] this))
