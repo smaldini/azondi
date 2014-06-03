bd1d195c0340e899b186d55858c328b104b7a549
Michael Klishin <michael@clojurewerkz.org>
2014-06-04 00:04:39 +0400

    Implement azondi.devices/device-names

--- src/clojure/azondi/devices.clj
+++ src/clojure/azondi/devices.clj
@@ -1,5 +1,6 @@
-(ns azondi.devices)
+(ns azondi.devices
+  (:require [clojure.java.jdbc :as j]))
 
 (defn device-names
   [conn ^String username]
-  [])
+  (map :name (j/query conn ["SELECT name FROM devices WHERE owner_user_id = ?" username])))
