e7a835fe55f7632e9b63ae2249574aea0a90944f
Michael Klishin <michael@clojurewerkz.org>
2014-06-19 09:17:14 +0400

    Make dev_system suitable for both development and test, force use of PG

--- dev/src/clojure/azondi/dev_system.clj
+++ dev/src/clojure/azondi/dev_system.clj
@@ -4,10 +4,7 @@
    [clojure.tools.logging :refer :all]
    [com.stuartsierra.component :as component]
    [azondi.system :refer (config configurable-system-map new-dependency-map new-prod-system)]
-   [azondi.api-tests :refer (new-api-tests)]
    [azondi.db :refer (Datastore get-user)]
-   [azondi.dev-db :refer (new-inmemory-datastore)]
-   [azondi.seed :refer (new-seed-data)]
    [azondi.postgres :refer (new-database)]
    [azondi.passwords :as pwd]
    [azondi.cassandra :as cass]))
@@ -17,30 +14,16 @@
   "Create a development system"
   [& [env]]
   (cond
-   (= env "prod") (let [c (config)
-                        s-map
-         (->
-          (configurable-system-map (config))
-          (assoc ;;:api-tests (azondi.api-tests/new-api-tests)
-                 ;;:seed (new-seed-data)
-
-                 :database (new-database (get c :postgres))
-
-                 :cassandra (cass/new-database (get c :cassandra {:keyspace "opensensors" :hosts ["127.0.0.1"]}))
-                 ))
-                       d-map (new-dependency-map s-map)]
-                   (component/system-using s-map d-map))
+   (= env "prod") (let [c     (config)
+                        s-map (-> (configurable-system-map (config))
+                                  (assoc :database (new-database (get c :postgres))
+                                         :cassandra (cass/new-database (get c :cassandra))))
+                        d-map (new-dependency-map s-map)]
+                    (component/system-using s-map d-map))
    :else
    (let [c (config)
-         s-map
-         (->
-          (configurable-system-map (config))
-          (assoc :seed (new-seed-data)
-                 :database (if (System/getenv "USE_POSTGRESQL")
-                             (new-database (get c :postgres))
-                             (new-inmemory-datastore))
-                 :cassandra (cass/new-database (get c :cassandra {:keyspace "opensensors" :hosts ["127.0.0.1"]}))
-                 ))
-
+         s-map (-> (configurable-system-map (config))
+                   (assoc :database (new-database (get c :postgres))
+                          :cassandra (cass/new-database (get c :cassandra))))
          d-map (new-dependency-map s-map)]
      (component/system-using s-map d-map))))
