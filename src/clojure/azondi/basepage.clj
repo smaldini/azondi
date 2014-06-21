b18f27f8cbacbbfb5df36071b30a8578253c14d2
Malcolm Sparks <malcolm@juxt.pro>
2014-06-21 16:49:32 +0100

    Menu items displayed according to logged in/out

--- src/clojure/azondi/basepage.clj
+++ src/clojure/azondi/basepage.clj
@@ -6,7 +6,7 @@
 
 (def menus
     [{:label "Devices"
-      :security :all
+      :security :user ; surely you need to have a username in order to create devices?
       :location :sidebar
       :target "/devices"}
      {:label "Topics"
@@ -46,7 +46,16 @@
       [:circle {:cx 400 :cy 30 :fill "red" :opacity "1.0" :r "1"}
        [:animate {:attributeName "r" :begin "1s" :calcmode "linear" :dur "1s" :values="15; 4; 2; 1"}]]]]]])
 
-(def side-menu
+(defn displayed? [menu session]
+  (case (:security menu)
+    :user session
+    :all true
+    :none (nil? session)
+    true
+    )
+  )
+
+(defn side-menu [session]
   [:div.navbar-default.navbar.sbar {:role "navigation"}
                      [:div.navbar-header.sbar
                       [:button {:type "button"
@@ -71,79 +80,87 @@
    ;;[:div {:id (str parent) :class "collapse out"}])
 
    [:ul
-    (for [menu menus]
+    (for [menu menus
+          :when (displayed? menu session)
+          ]
       (when (= :sidebar (:location menu))
         [:li.side-menu-item [:a {:href (:target menu)} (:label menu)]]))]])
 
+
+
 (defn base-page [req body & scr]
-  (html5
-   [:head
-    [:meta {:charset "utf-8"}]
-    [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"}]
-    [:meta {:property "dc:language" :content "en"}]
-    [:meta {:property "dc:title" :content "opensensors.IO"}]
-    [:meta {:property "dc:description" :content "opensensors.IO processes sensor data using azondi"}]
-    [:title "opensensors.IO"]
-    [:link {:href "//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" :rel "stylesheet"}]
-    [:link {:href "css/bootstrap.min.css" :rel "stylesheet"}]
-    [:link {:href "css/style.css" :rel "stylesheet"}]
+  (let [session (:session req)]
+    (html5
+     [:head
+      [:meta {:charset "utf-8"}]
+      [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"}]
+      [:meta {:property "dc:language" :content "en"}]
+      [:meta {:property "dc:title" :content "opensensors.IO"}]
+      [:meta {:property "dc:description" :content "opensensors.IO processes sensor data using azondi"}]
+      [:title "opensensors.IO"]
+      [:link {:href "//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" :rel "stylesheet"}]
+      [:link {:href "css/bootstrap.min.css" :rel "stylesheet"}]
+      [:link {:href "css/style.css" :rel "stylesheet"}]
 
-   [:body
-    [:div#wrap
-     [:nav {:class "navbar navbar-default" :role "navigation"}
-      [:div.container-fluid
-       [:div.navbar-header
-        [:button.navbar-toggle {:type "buton" :data-toggle "collapse" :data-target "#bs-example-navbar-collapse-1"}
-         [:span.sr-only "Toggle navigation"]
-         [:span.icon-bar]
-         [:span.icon-bar]
-         [:span.icon-bar]]
-        [:a#home-logo.navbar-brand {:href "/"} [:img {:src "imgs/osio.svg"}]]
-        [:ul {:class "nav navbar-nav"}
-         [:li [:a {:href "/services"} "Services"]]]]
-       [:div {:class "collapse navbar-collapse" :id "bs-example-navbar-collapse-1"}
-        [:ul {:class "nav navbar-nav navbar-right"}
-         (for [menu menus]
-           (if (= :navbar (:location menu))
-             (if (:children menu)
-               [:li.dropdown [:a.dropdown-toggle {:href "#" :data-toggle "dropdown"} (:label menu) [:b.caret]]
-                [:ul.dropdown-menu
-                 (for [child (:children menu)]
-                   [:li [:a {:href (:target child)} (:label child)]])]]
-               [:li [:a {:href (:target menu)} (:label menu)]]
-               )))
-         [:li [:a (or (:cylon/user req) "USER")]]]]]]
-     logo-area
-     [:div.row
-      [:div.col-sm-2
-       [:div.sidebar-nav
-        side-menu]]
-      [:div.col-sm-9
-       body]
-      [:div#ankha]]
-     [:script {:src "js/jquery.min.js"}]
-     [:script {:src "js/helpers.js"}]
-     [:script {:src "js/bootstrap.min.js"}]
-     [:script {:src "js/react-0.9.0.js"}]
-     ;;cljs
-     [:script {:src "cljs/cljs.js"}]
-     [:script {:src "cljs/azondi.js"}]
-     [:script {:src "cljs/logo.js"}]]
-     [:div#footer {:class "navbar-default navbar-fixed-bottom"}
-      [:ul.footer-list
-       [:li "&copy; 2014 open sensors ltd"]
-       [:li [:a {:href "/about"} "About Us"]]
-       [:li [:a {:href "http://blog.opensensors.IO"} "Blog"]]
-       [:li [:a {:href "/terms"} "Terms"]]
-       [:li [:a {:href "https://twitter.com/opensensorsio"}
-             [:img {:src "imgs/glyphicons_social_31_twitter.png"}]]]
-       [:li [:a {:href "mailto:hello@opensensors.io?subject=website%20enquiry"}
-             [:img {:src "imgs/glyphicons_010_envelope.png"}]]]]]
-      ;; extenal libs
+      [:body
+       [:div#wrap
+        [:nav {:class "navbar navbar-default" :role "navigation"}
+         [:div.container-fluid
+          [:div.navbar-header
+           [:button.navbar-toggle {:type "buton" :data-toggle "collapse" :data-target "#bs-example-navbar-collapse-1"}
+            [:span.sr-only "Toggle navigation"]
+            [:span.icon-bar]
+            [:span.icon-bar]
+            [:span.icon-bar]]
+           [:a#home-logo.navbar-brand {:href "/"} [:img {:src "imgs/osio.svg"}]]
+           [:ul {:class "nav navbar-nav"}
+            [:li [:a {:href "/services"} "Services"]]]]
+          [:div {:class "collapse navbar-collapse" :id "bs-example-navbar-collapse-1"}
+           [:ul {:class "nav navbar-nav navbar-right"}
+            (for [menu menus
+                  :when (displayed? menu session)]
+              (if (= :navbar (:location menu))
+                (if (:children menu)
+                  [:li.dropdown [:a.dropdown-toggle {:href "#" :data-toggle "dropdown"} (:label menu) [:b.caret]]
+                   [:ul.dropdown-menu
+                    (for [child (:children menu)
+                          :when (displayed? child session)]
+                      [:li [:a {:href (:target child)} (:label child)]])]]
+                  [:li [:a {:href (:target menu)} (:label menu)]]
+                  )))
+            (when-let [user (:cylon/user req)]
+              [:li [:a user]])]]]]
+        logo-area
+        [:div.row
+         [:div.col-sm-2
+          [:div.sidebar-nav
+           (side-menu session)]]
+         [:div.col-sm-9
+          body]
+         [:div#ankha]]
+        [:script {:src "js/jquery.min.js"}]
+        [:script {:src "js/bootstrap.min.js"}]
+        [:script {:src "js/react-0.9.0.js"}]
+        ;;cljs
+        [:script {:src "cljs/cljs.js"}]
+        [:script {:src "cljs/azondi.js"}]
+        [:script {:src "cljs/logo.js"}]
+        [:script {:src "js/helpers.js"}]]
+       [:div#footer {:class "navbar-default navbar-fixed-bottom"}
+        [:ul.footer-list
+         [:li "&copy; 2014 open sensors ltd"]
+         [:li [:a {:href "/about"} "About Us"]]
+         [:li [:a {:href "http://blog.opensensors.IO"} "Blog"]]
+         [:li [:a {:href "/terms"} "Terms"]]
+         [:li [:a {:href "https://twitter.com/opensensorsio"}
+               [:img {:src "imgs/glyphicons_social_31_twitter.png"}]]]
+         [:li [:a {:href "mailto:hello@opensensors.io?subject=website%20enquiry"}
+               [:img {:src "imgs/glyphicons_010_envelope.png"}]]]]]
+       ;; extenal libs
 
-     ]
-     scr
-    ]))
+       ]
+      scr
+      ])))
 
 ;;; We need to pull out the user details from the session
 (defn devices-page [req]
