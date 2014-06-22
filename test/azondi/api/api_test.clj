432e3e7af87eab105c657a68495f446cc8bb6695
Malcolm Sparks <malcolm@juxt.pro>
2014-06-22 09:37:54 +0100

    3 simple API tests

--- test/azondi/api/api_test.clj
+++ test/azondi/api/api_test.clj
@@ -4,33 +4,39 @@
    [com.stuartsierra.component :as component]
    [modular.http-kit :refer (new-webserver)]
    [modular.ring :refer (new-web-request-handler-head)]
-   [modular.bidi :refer (new-router)]
+   [modular.bidi :refer (new-router routes uri-context)]
+   [bidi.bidi :refer (path-for)]
    [azondi.webapp :refer (new-webapp)]
    [azondi.api :refer (new-api)]
    [cylon.impl.login-form :refer (new-login-form)]
+   [azondi.http :refer (request)]
+   [azondi.dev-db :refer (new-inmemory-datastore)]
    ))
 
 (defn new-api-system []
   (component/system-using
 
-   {:webserver (new-webserver :port 8099)
+   (component/system-map
+    :webserver (new-webserver :port 8099)
     :webhead (new-web-request-handler-head)
     :webrouter (new-router)
-    :webapp (new-webapp)
-    :api (new-api :uri-context "/api/1.0")}
+    :database (new-inmemory-datastore)
+    :api (new-api :uri-context "/api/1.0"))
 
    {:webserver {:request-handler :webhead}
-    :webhead {:request-handler :webrouter, :authenticator-middleware :authenticator}
-    :webrouter [:webapp :api]}))
+    :webhead {:request-handler :webrouter,
+              #_:authenticator-middleware #_:authenticator}
+    :webrouter [:api]}))
 
 (def ^:dynamic *system* nil)
 
 (defmacro with-system [system & body]
-  `(let [s# ~system]
+  `(let [s# (component/start ~system)]
      (try
-       (component/start s#)
        (binding [*system* s#] ~@body)
        (finally
+         (println "System stopping!")
+         ;;(Thread/sleep 4000)
          (component/stop s#)))))
 
 (defn system-fixture [f]
@@ -39,7 +45,29 @@
 
 (use-fixtures :once system-fixture)
 
+(defn make-uri [target & args]
+  (format "http://localhost:%d%s%s"
+          (-> *system* :webserver :port)
+          (uri-context (-> *system* :api))
+          (apply path-for (routes (-> *system* :api)) target args)))
+
 (deftest server
-  (testing "webserver"
-    (println "System is " (type *system*))
-    (is (= (+ 2 2) 4))))
+  (testing "control"
+    (is (= (+ 2 2) 4)))
+
+  (testing "welcome path"
+    (is (= (make-uri :azondi.api/welcome)
+           "http://localhost:8099/api/1.0")))
+
+  (testing "user path"
+    (is (= (make-uri :azondi.api/user :user "alice")
+           "http://localhost:8099/api/1.0/users/alice")))
+
+  (testing "create user"
+    (request :put (make-uri :azondi.api/user :user "alice")
+             :data {:password "lewis"
+                    :name "Alice Cheung"
+                    :email "alice@example.org"
+                    })
+
+    ))
