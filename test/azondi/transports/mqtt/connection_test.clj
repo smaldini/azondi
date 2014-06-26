9611b657fcfd5612ceb59ad96f1056340631d56f
Michael Klishin <michael@clojurewerkz.org>
2014-06-26 21:24:01 +0400

    Cosmetics

--- test/azondi/transports/mqtt/connection_test.clj
+++ test/azondi/transports/mqtt/connection_test.clj
@@ -4,8 +4,7 @@
             [clojurewerkz.machine-head.client :as mh]
             [clojurewerkz.machine-head.durability :as md]
             [azondi.test-helpers :as th])
-  (:import java.util.concurrent.atomic.AtomicInteger
-           java.util.UUID))
+  (:import java.util.UUID))
 
 (use-fixtures :once th/maybe-load-schema-fixture)
 (use-fixtures :each th/with-system-fixture)
