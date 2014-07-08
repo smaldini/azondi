afa6569b863d739c2664c7a2a0473f0753397468
Malcolm Sparks <malcolm@juxt.pro>
2014-07-08 12:00:47 +0100

    logo not working

--- project.clj
+++ project.clj
@@ -76,6 +76,7 @@
                  [om "0.6.0"]
 
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+                 [prismatic/dommy "0.1.2"]
                  [jarohen/chord "0.3.1"]
 
                  ]
