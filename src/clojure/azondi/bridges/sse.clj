826c92cc693dfdc03fd6b58299119fb779cff066
Malcolm Sparks <malcolm@juxt.pro>
2014-08-04 15:50:21 +0100

    SSE security and topic subscription behavior
    SSE firehose is now authorized via the api-key mechanism, and will also
    be available to the browser.
    
    As specified, only subscribed topics are published through the fire-hose

--- src/clojure/azondi/bridges/sse.clj
+++ src/clojure/azondi/bridges/sse.clj
@@ -1,6 +1,7 @@
 (ns azondi.bridges.sse
   (:require
    [com.stuartsierra.component :refer (using)]
+   [azondi.db :refer (subscriptions-by-owner)]
    [bidi.bidi :refer (->Redirect)]
    [clojure.tools.logging :refer :all]
    [schema.core :as s]
@@ -10,41 +11,56 @@
    [clojure.java.io :as io]
    [clojurewerkz.meltdown.reactor :as mr]
    [clojurewerkz.meltdown.consumers :as mc]
-   [clojurewerkz.meltdown.selectors :refer (set-membership)]))
+   [clojurewerkz.meltdown.selectors :refer (set-membership predicate)]
+   [cylon.authentication :refer (authenticate)]))
 
 (defn read-bytes [bs cs]
   (slurp (io/reader bs :encoding cs)))
 
-(defn server-event-source [reactor]
+(defn server-event-source [reactor authorizer database]
   (fn [{{prefix :prefix} :route-params :as req}]
-    (with-channel req channel
-      (send! channel
-             {:headers {"Content-Type" "text/event-stream"}} false)
-      (debugf "Opening firehose (prefix:%s)" prefix)
-      (let [rsub
-            (mr/on
-             reactor
-             (set-membership #{"messages.published"})
-             (fn [evt]
-               (when
-                   (when-let [topic (-> evt :data :topic)]
-                     (.startsWith topic prefix))
-                 (send! channel
-                        (let [{:keys [charset] :as data} (:data evt)]
-                          (str "data: "
-                               (cond-> data
-                                       charset
-                                       (update-in [:payload] read-bytes charset))
-                               "\r\n\r\n"))
-                        false))))]
-        (on-close channel (fn [status]
-                            (debugf "Closing firehose")
-                            (mc/cancel rsub)))))))
 
-(defrecord ServerSentEventBridge [uri-context reactor]
+    (let [user (:cylon/user (authenticate (:authenticator authorizer) req))]
+      (if user
+        (with-channel req channel
+          (send! channel
+                 {:headers {"Content-Type" "text/event-stream"}} false)
+          (debugf "Opening firehose (prefix:%s)" prefix)
+          (debugf "Subscriptions are: %s" (apply str (map :topic (subscriptions-by-owner database user))))
+          (let [subscribed-topics (set (map :topic (subscriptions-by-owner database user)))
+                rsub
+                (mr/on
+                 reactor
+                 ;; QUESTION (for MK): it appears from ws.clj that we
+                 ;; should be able to select on topics, instead of these
+                 ;; general 'messages.published' key - is this the
+                 ;; ultimate goal?
+                 (set-membership #{"messages.published"})
+                 (fn [evt]
+                   (when
+                       (when-let [topic (-> evt :data :topic)]
+                         (and
+                          (contains? subscribed-topics topic)
+                          (.startsWith topic prefix)))
+                     (send! channel
+                            (let [{:keys [charset] :as data} (:data evt)]
+                              (str "data: "
+                                   (cond-> data
+                                           charset
+                                           (update-in [:payload] read-bytes charset))
+                                   "\r\n\r\n"))
+                            false))))]
+            (on-close channel (fn [status]
+                                (debugf "Closing firehose")
+                                (mc/cancel rsub)))))
+        {:status 403
+         :body "Unauthorized access to event stream"}
+        ))))
+
+(defrecord ServerSentEventBridge [uri-context reactor authorizer database]
   WebService
   (request-handlers [component]
-    {::events (server-event-source (:reactor reactor))})
+    {::events (server-event-source (:reactor reactor) authorizer database)})
   (routes [component]
     ["" [[["/events" [#".*" :prefix]] ::events]]])
   (uri-context [component]
@@ -55,4 +71,4 @@
        (merge {:uri-context ""})
        (s/validate {:uri-context s/Str})
        map->ServerSentEventBridge
-       (<- (using [:reactor]))))
+       (<- (using [:reactor :authorizer :database]))))
