2723908548e7a8d092b725722ef41c813fd83b6d
Malcolm Sparks <malcolm@juxt.pro>
2014-08-20 08:57:45 +0100

    Shutdown of go blocks in simulator

--- dev/src/clojure/azondi/simulator.clj
+++ dev/src/clojure/azondi/simulator.clj
@@ -6,7 +6,7 @@
    [schema.core :as s]
    [azondi.db :refer (create-device! allowed-device?)]
    [plumbing.core :refer (<-)]
-   [clojure.core.async :refer (go-loop timeout)]))
+   [clojure.core.async :refer (go-loop timeout chan alts! close!)]))
 
 (defrecord Simulator [database]
   Lifecycle
@@ -21,21 +21,25 @@
         (infof "New device created for simulator: %s" device)
         (assert (allowed-device? database client-id user password))
 
+        (let [test-device (create-device! database user "abc123")]
+          (infof "mosquitto_sub -h %s -i %s -t /users/%s/test -u %s -P %s"
+                 "localhost" (:client-id test-device) user user (:password test-device)))
+
+
         (let [client (mh/connect "tcp://localhost:1883" client-id
                                  (new org.eclipse.paho.client.mqttv3.persist.MemoryPersistence)
                                  {:username user
                                   :password password})]
 
-          (go-loop []
-            (<! (timeout 100))
-            (mh/publish client (format "/users/%s/test" user) (.getBytes "Hello!!!") 0)
-            (recur))
+          (let [shutdown (chan)]
+            (go-loop []
+              (let [[_ ch] (alts! [shutdown (timeout 1000)])]
+                (when-not (= ch shutdown)
+                  (mh/publish client (format "/users/%s/test" user) (.getBytes "Hello!!!") 0)
+                  (recur))))
 
-          (assoc component :client client))
+            (assoc component :client client :shutdown shutdown))))
 
-        component
-
-        )
       (catch Exception e
           (println "Failed to start Simulator component - see log")
           (errorf e "Failed to start Simulator component")
@@ -43,6 +47,8 @@
           )
       ))
   (stop [component]
+    (when-let [shutdown (:shutdown component)]
+      (close! shutdown))
     (when-let [client (:client component)]
       (mh/disconnect client))
     component))
