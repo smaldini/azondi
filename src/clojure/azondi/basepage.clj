ab0cf820ccb52474511b2c2f327bf50d88801ddb
yods <yodit_berga@yahoo.co.uk>
2014-08-25 00:47:31 +0100

    get rid of clicky link

--- src/clojure/azondi/basepage.clj
+++ src/clojure/azondi/basepage.clj
@@ -209,7 +209,7 @@
                                   ;; extenal libs
     scr
     ;; analytics
-     "<a title=\"Real Time Analytics\" href=\"http://clicky.com/100765453\"><img alt=\"Real Time Analytics\" src=\"//static.getclicky.com/media/links/badge.gif\" border=\"0\" /></a>
+     "
 <script src=\"//static.getclicky.com/js\" type=\"text/javascript\"></script>
 <script type=\"text/javascript\">try{ clicky.init(100765453); }catch(e){}</script>
 <noscript><p><img alt=\"Clicky\" width=\"1\" height=\"1\" src=\"//in.getclicky.com/100765453ns.gif\" /></p></noscript>
