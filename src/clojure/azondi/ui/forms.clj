b3f2c7e66107ce72a166bb075e97baf29fa7f88b
Malcolm Sparks <malcolm@juxt.pro>
2014-10-02 17:55:15 +0100

    Login form integration
    Succeeding but no redirection yet

--- src/clojure/azondi/ui/forms.clj
+++ src/clojure/azondi/ui/forms.clj
@@ -4,14 +4,35 @@
    [schema.core :as s]
    [cylon.authentication.login :refer (LoginFormRenderer)]
    [cylon.signup.protocols :refer (SignupFormRenderer WelcomeRenderer)]
-   [clostache.parser :refer (render-resource)]))
+   [clojure.walk :refer (postwalk)]
+   [clostache.parser :refer (render-resource)]
+   [clojure.tools.logging :refer :all]))
 
+(defprotocol TemplateDataValue
+  (as-template-data-value [_]
+    "Turn Clojure things into strings (useful for a Mustache template model)"))
+
+(extend-protocol TemplateDataValue
+  nil
+  (as-template-data-value [_] "")
+  clojure.lang.Keyword
+  (as-template-data-value [k] (name k))
+  Object
+  (as-template-data-value [s] s ))
+
+(defn stringify-map-values [a]
+  (if (and (vector? a) (= (count a) 2))
+    [(first a) (as-template-data-value (second a))]
+    a))
 
 (defrecord OsioUserFormRenderer []
   LoginFormRenderer
   (render-login-form [this req model]
+    (infof "Model is %s" (postwalk stringify-map-values model))
     (render-resource "templates/boilerplate.html.mustache"
-                     {:content (render-resource "templates/login.html.mustache" {})}))
+                     {:content (render-resource "templates/login.html.mustache"
+                                                (postwalk stringify-map-values model))}))
+
   SignupFormRenderer
   (render-signup-form [this req model]
     ;; TODO
