11b2b65e21f4413ac35f8605b03ca76ce105a7f2
Michael Klishin <michael@clojurewerkz.org>
2014-10-04 01:35:42 +0400

    Facepalm

--- dev/src/clojure/azondi/seeds/cassandra.clj
+++ dev/src/clojure/azondi/seeds/cassandra.clj
@@ -23,17 +23,17 @@
           o "juan"]
       (let [now (tc/date-time 2014 1 15 0)]
         (cql/insert conn t {:device_id "1001" :topic "/users/juan/test-public" :owner o
-                            :payload (bytes "message1") :content-type "text"
+                            :payload (bytes "message1") :content_type "text"
                             :created_at (.toDate now) :date_and_hour (tf/unparse date-and-hour-formatter now)}))
       (let [now (tc/date-time 2014 1 16 0)]
         (cql/insert conn t {:device_id "1001" :topic "/users/juan/test-public" :owner o
-                            :payload (bytes "message2") :content-type "text"
+                            :payload (bytes "message2") :content_type "text"
                             :created_at (.toDate now) :date_and_hour (tf/unparse date-and-hour-formatter now)}))
       (let [now (tc/date-time 2014 1 17 0)]
         (cql/insert conn t {:device_id "1002" :topic "/users/juan/test-private" :owner o
-                            :payload (bytes "message3") :content-type "text"
+                            :payload (bytes "message3") :content_type "text"
                             :created_at (.toDate now) :date_and_hour (tf/unparse date-and-hour-formatter now)}))
       (let [now (tc/date-time 2014 1 18 0)]
         (cql/insert conn t {:device_id "1002" :topic "/users/juan/test-private" :owner o
-                            :payload (bytes "message4") :content-type "text"
+                            :payload (bytes "message4") :content_type "text"
                             :created_at (.toDate now) :date_and_hour (tf/unparse date-and-hour-formatter now)})))))
