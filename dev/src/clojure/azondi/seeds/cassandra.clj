ec925a3428a0685482b5999f6d1c1533c35a6efb
Michael Klishin <michael@clojurewerkz.org>
2014-10-04 19:22:46 +0400

    Use an actual content type here

--- dev/src/clojure/azondi/seeds/cassandra.clj
+++ dev/src/clojure/azondi/seeds/cassandra.clj
@@ -23,25 +23,25 @@
           o "juan"]
       (let [now (tc/date-time 2014 1 15 0)]
         (cql/insert conn t {:device_id "1001" :topic "/users/juan/test-public" :owner o
-                            :payload (bytes "message1") :content_type "text"
+                            :payload (bytes "message1") :content_type "text/plain"
                             :created_at (.toDate now) :date_and_hour (tf/unparse date-and-hour-formatter now)}))
       (let [now (tc/date-time 2014 1 16 0)]
         (cql/insert conn t {:device_id "1001" :topic "/users/juan/test-public" :owner o
-                            :payload (bytes "message2") :content_type "text"
+                            :payload (bytes "message2") :content_type "text/plain"
                             :created_at (.toDate now) :date_and_hour (tf/unparse date-and-hour-formatter now)}))
       (let [now (tc/date-time 2014 1 17 0)]
         (cql/insert conn t {:device_id "1002" :topic "/users/juan/test-private" :owner o
-                            :payload (bytes "message3") :content_type "text"
+                            :payload (bytes "message3") :content_type "text/plain"
                             :created_at (.toDate now) :date_and_hour (tf/unparse date-and-hour-formatter now)}))
       (let [now (tc/date-time 2014 1 18 0)]
         (cql/insert conn t {:device_id "1002" :topic "/users/juan/test-private" :owner o
-                            :payload (bytes "message4") :content_type "text"
+                            :payload (bytes "message4") :content_type "text/plain"
                             :created_at (.toDate now) :date_and_hour (tf/unparse date-and-hour-formatter now)}))
       (let [now (tc/date-time 2014 9 18 0)]
         (cql/insert conn t {:device_id "4" :topic "/users/michaelklishin/topic1" :owner "michaelklishin"
-                            :payload (bytes "message") :content_type "text"
+                            :payload (bytes "message") :content_type "text/plain"
                             :created_at (.toDate now) :date_and_hour (tf/unparse date-and-hour-formatter now)}))
       (let [now (tc/date-time 2014 9 18 1)]
         (cql/insert conn t {:device_id "4" :topic "/users/michaelklishin/topic1" :owner "michaelklishin"
-                            :payload (bytes "message") :content_type "text"
+                            :payload (bytes "message") :content_type "text/plain"
                             :created_at (.toDate now) :date_and_hour (tf/unparse date-and-hour-formatter now)})))))
